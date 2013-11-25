14077f4d6bb22aac618c6fadc6311d04c843a036
Nejc Zdovc <nejc.zdovc@3zsistemi.si>
2013-11-25 12:46:36 +0100

    Javascript now check if element exists.

--- includes/fields.class.php
+++ includes/fields.class.php
@@ -13,9 +13,10 @@ class Fields
 		global $db;
 		if($required=="1") {
 		$this->javascript.="var element=document.getElementById('".$fieldId."');
+		if (element)  {
 		 if(element.value.length<".$min." || element.value.length>".$max.") {ok=false;element.className=element.className.replace('error','');element.className+='error';} 
 		 else
-		  {element.className=element.className.replace('error','');}";
+		  {element.className=element.className.replace('error','');}}";
 		}
 		if($db->is($fieldId))
 			return '<input type="text" maxlength="'.$max.'" value="'.$db->filterVar($fieldId).'" name="'.$fieldId.'" id="'.$fieldId.'" />';		
@@ -29,10 +30,13 @@ class Fields
 		global $db;
 		if($required=="0") {
 			$this->javascript.="var element=document.getElementById('".$fieldId."'); var reg = /^([a-zA-Z0-9\u00A1-\uFFFF]+([\.+_-][a-zA-Z0-9\u00A1-\uFFFF]+)*)@(([a-zA-Z0-9]+((\.|[-]{1,2})[a-zA-Z0-9]+)*)\.[a-zA-Z]{2,6})$/;
-			if(element.value.length>0 && !reg.test(element.value)) {ok=false;element.className=element.className.replace('error','');element.className+='error';} else {element.className=element.className.replace('error','');}";
+            console.log(typeof(element));
+			if (element)  {
+			if(element.value.length>0 && !reg.test(element.value)) {ok=false;element.className=element.className.replace('error','');element.className+='error';} else {element.className=element.className.replace('error','');}}";
 		} else {
 			$this->javascript.="var element=document.getElementById('".$fieldId."'); var reg = /^([a-zA-Z0-9\u00A1-\uFFFF]+([\.+_-][a-zA-Z0-9\u00A1-\uFFFF]+)*)@(([a-zA-Z0-9]+((\.|[-]{1,2})[a-zA-Z0-9]+)*)\.[a-zA-Z]{2,6})$/;
-			if(!reg.test(element.value)) {ok=false;element.className=element.className.replace('error','');element.className+='error';} else {element.className=element.className.replace('error','');}";
+			if (element)  {
+			if(!reg.test(element.value)) {ok=false;element.className=element.className.replace('error','');element.className+='error';} else {element.className=element.className.replace('error','');}}";
 		}
 		if($db->is($fieldId))
 			return '<input type="text" name="'.$fieldId.'" id="'.$fieldId.'" value="'.$db->filterVar($fieldId).'" />';
@@ -45,7 +49,7 @@ class Fields
 	public function password($min, $fieldId, $required, $value=null) {
 		global $db;
 		if($required=="1") {
-		$this->javascript.="var element=document.getElementById('".$fieldId."'); if(element.value.length<".$min.") {ok=false;element.className=element.className.replace('error','');element.className+='error';} else {element.className=element.className.replace('error','');}";
+		$this->javascript.="var element=document.getElementById('".$fieldId."'); if (element)  {if(element.value.length<".$min.") {ok=false;element.className=element.className.replace('error','');element.className+='error';} else {element.className=element.className.replace('error','');}}";
 		}
 		if($db->is($fieldId))
 			return '<input type="password" value="'.$db->filterVar($fieldId).'" name="'.$fieldId.'" id="'.$fieldId.'" />';		
@@ -61,7 +65,7 @@ class Fields
 		$code="";
 		$stevilo=0;
 		if($required=="1") {
-			$this->javascript.="var element=document.getElementsByName('".$fieldId."[]');var napaka=true; for (i = 0; i < element.length; i++) {if(element[i].checked) {napaka=false;}} if(napaka){ok=false;for (i = 0; i < element.length; i++) {document.getElementById(element[i].id+'_label').className=element[i].className.replace('error_text','');document.getElementById(element[i].id+'_label').className+=' error_text';}} else {for (i = 0; i < element.length; i++) {document.getElementById(element[i].id+'_label').className=document.getElementById(element[i].id+'_label').className.replace('error_text','');}}";	
+			$this->javascript.="var element=document.getElementsByName('".$fieldId."[]');if (element)  {var napaka=true; for (i = 0; i < element.length; i++) {if(element[i].checked) {napaka=false;}} if(napaka){ok=false;for (i = 0; i < element.length; i++) {document.getElementById(element[i].id+'_label').className=element[i].className.replace('error_text','');document.getElementById(element[i].id+'_label').className+=' error_text';}} else {for (i = 0; i < element.length; i++) {document.getElementById(element[i].id+'_label').className=document.getElementById(element[i].id+'_label').className.replace('error_text','');}}}";	
 		}
 		foreach($options as $i) {
 			if(isset($_REQUEST[$fieldId]) && in_array($i, $_REQUEST[$fieldId]))	{	
@@ -84,9 +88,10 @@ class Fields
 		global $db;
 		if($required=="1") {
 		$this->javascript.="var element=document.getElementById('".$fieldId."');
+		if (element)  {
 		 if(element.value.length<".$min." || element.value.length>".$max.") {ok=false;element.className=element.className.replace('error','');element.className+='error';} 
 		 else
-		  {element.className=element.className.replace('error','');}";
+		  {element.className=element.className.replace('error','');}}";
 		}
 		if($db->is($fieldId))
 			return '<textarea maxlength="'.$max.'" name="'.$fieldId.'" id="'.$fieldId.'">'.$db->filterVar($fieldId).'"</textarea>';		
