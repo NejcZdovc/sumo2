0126ae37b46247fbb26c63dbe4025d3e1a73542c
Nejc Zdovc <nejc@digitalship.eu>
2014-03-06 18:28:36 +0100

    Fixed field validation so that fields can be now defined for form

--- includes/fields.class.php
+++ includes/fields.class.php
@@ -9,14 +9,18 @@ class Fields
 {	
 	public $javascript="";
 	
-	public function input($min, $max, $fieldId, $required="0", $value=null, $dynamic=false) {
+	public function input($min, $max, $fieldId, $required="0", $value=null, $dynamic=false, $form=null) {
 		global $db;
 		if($required=="1") {
-		$this->javascript.="var element=document.getElementById('".$fieldId."');
-		if (element)  {
-		 if(element.value.length<".$min." || element.value.length>".$max.") {ok=false;element.className=element.className.replace('error','');element.className+='error';} 
-		 else
-		  {element.className=element.className.replace('error','');}}";
+            if($form!=null) {
+                $this->javascript.="var element=document.".$form.".".$fieldId.";";
+            } else {
+               $this->javascript.="var element=document.getElementById('".$fieldId."');"; 
+            }
+            $this->javascript.="if (element)  {
+             if(element.value.length<".$min." || element.value.length>".$max.") {ok=false;element.className=element.className.replace('error','');element.className+='error';} 
+             else
+              {element.className=element.className.replace('error','');}}";
 		}
 		if($dynamic && $db->is($fieldId))
 			return '<input type="text" maxlength="'.$max.'" value="'.$db->filter($fieldId).'" name="'.$fieldId.'" id="'.$fieldId.'" />';		
@@ -26,15 +30,18 @@ class Fields
 			return '<input type="text" maxlength="'.$max.'" name="'.$fieldId.'" id="'.$fieldId.'" />';		
 	}
 	
-	public function email($fieldId, $value=null, $required="0", $dynamic=false) {
+	public function email($fieldId, $value=null, $required="0", $dynamic=false, $form=null) {
 		global $db;
+        if($form!=null) {
+             $this->javascript.="var element=document.".$form.".".$fieldId."; var reg = /^([a-zA-Z0-9\u00A1-\uFFFF]+([\.+_-][a-zA-Z0-9\u00A1-\uFFFF]+)*)@(([a-zA-Z0-9]+((\.|[-]{1,2})[a-zA-Z0-9]+)*)\.[a-zA-Z]{2,6})$/;";
+        } else {
+            $this->javascript.="var element=document.getElementById('".$fieldId."'); var reg = /^([a-zA-Z0-9\u00A1-\uFFFF]+([\.+_-][a-zA-Z0-9\u00A1-\uFFFF]+)*)@(([a-zA-Z0-9]+((\.|[-]{1,2})[a-zA-Z0-9]+)*)\.[a-zA-Z]{2,6})$/;";
+        }
 		if($required=="0") {
-			$this->javascript.="var element=document.getElementById('".$fieldId."'); var reg = /^([a-zA-Z0-9\u00A1-\uFFFF]+([\.+_-][a-zA-Z0-9\u00A1-\uFFFF]+)*)@(([a-zA-Z0-9]+((\.|[-]{1,2})[a-zA-Z0-9]+)*)\.[a-zA-Z]{2,6})$/;
-			if (element)  {
+			$this->javascript.="{
 			if(element.value.length>0 && !reg.test(element.value)) {ok=false;element.className=element.className.replace('error','');element.className+='error';} else {element.className=element.className.replace('error','');}}";
 		} else {
-			$this->javascript.="var element=document.getElementById('".$fieldId."'); var reg = /^([a-zA-Z0-9\u00A1-\uFFFF]+([\.+_-][a-zA-Z0-9\u00A1-\uFFFF]+)*)@(([a-zA-Z0-9]+((\.|[-]{1,2})[a-zA-Z0-9]+)*)\.[a-zA-Z]{2,6})$/;
-			if (element)  {
+			$this->javascript.=" if (element)  {
 			if(!reg.test(element.value)) {ok=false;element.className=element.className.replace('error','');element.className+='error';} else {element.className=element.className.replace('error','');}}";
 		}
 		if($dynamic && $db->is($fieldId))		
@@ -45,10 +52,15 @@ class Fields
 			return '<input type="text" name="'.$fieldId.'" id="'.$fieldId.'"/>';
 	}
 	
-	public function password($min, $fieldId, $required, $value=null, $dynamic=false) {
+	public function password($min, $fieldId, $required, $value=null, $dynamic=false, $form=null) {
 		global $db;
 		if($required=="1") {
-		$this->javascript.="var element=document.getElementById('".$fieldId."'); if (element)  {if(element.value.length<".$min.") {ok=false;element.className=element.className.replace('error','');element.className+='error';} else {element.className=element.className.replace('error','');}}";
+             if($form!=null) {
+                $this->javascript.="var element=document.".$form.".".$fieldId.";";
+             } else {
+                $this->javascript.="var element=document.getElementById('".$fieldId."');";
+             }
+            $this->javascript.="if (element)  {if(element.value.length<".$min.") {ok=false;element.className=element.className.replace('error','');element.className+='error';} else {element.className=element.className.replace('error','');}}";
 		}
 		if($dynamic && $db->is($fieldId))
 			return '<input type="password" value="'.$db->filter($fieldId).'" name="'.$fieldId.'" id="'.$fieldId.'" />';		
@@ -58,13 +70,18 @@ class Fields
 			return '<input type="password" name="'.$fieldId.'" id="'.$fieldId.'" />';		
 	}
 	
-	public function check($fieldId, $options, $required, $position, $value=null, $dynamic=false) {
+	public function check($fieldId, $options, $required, $position, $value=null, $dynamic=false, $form=null) {
 		global $db;
 		$options=explode(',', $options);
 		$code="";
 		$stevilo=0;
 		if($required=="1") {
-			$this->javascript.="var element=document.getElementsByName('".$fieldId."[]');if (element)  {var napaka=true; for (i = 0; i < element.length; i++) {if(element[i].checked) {napaka=false;}} if(napaka){ok=false;for (i = 0; i < element.length; i++) {document.getElementById(element[i].id+'_label').className=element[i].className.replace('error_text','');document.getElementById(element[i].id+'_label').className+=' error_text';}} else {for (i = 0; i < element.length; i++) {document.getElementById(element[i].id+'_label').className=document.getElementById(element[i].id+'_label').className.replace('error_text','');}}}";	
+            if($form!=null) {
+                $this->javascript.="var element=document.forms['".$form."'].elements['".$fieldId."[]'];";
+            } else {
+                $this->javascript.="var element=document.getElementsByName('".$fieldId."[]');";
+            }
+             $this->javascript.="if (element)  {var napaka=true; for (i = 0; i < element.length; i++) {if(element[i].checked) {napaka=false;}} if(napaka){ok=false;for (i = 0; i < element.length; i++) {document.getElementById(element[i].id+'_label').className=element[i].className.replace('error_text','');document.getElementById(element[i].id+'_label').className+=' error_text';}} else {for (i = 0; i < element.length; i++) {document.getElementById(element[i].id+'_label').className=document.getElementById(element[i].id+'_label').className.replace('error_text','');}}}";	
 		}
 		foreach($options as $i) {
 			if($dynamic && isset($_REQUEST[$fieldId]) && in_array($i, $_REQUEST[$fieldId]))	{	
@@ -79,18 +96,25 @@ class Fields
 					$code.='<input type="checkbox" name="'.$fieldId.'[]" class="'.$fieldId.'" id="'.$fieldId.'_'.$stevilo.'" value="'.$i.'" /><label for="'.$fieldId.'_'.$stevilo.'" id="'.$fieldId.'_'.$stevilo.'_label">'.$i.'</label>';		
 			}
 			$stevilo++;		
-		}		
+		}
+        if(count($options)==1 && $form!=null) {
+            $code.='<input type="checkbox" disabled="disabled" style="display:none !important;" name="'.$fieldId.'[]" class="'.$fieldId.'" id="'.$fieldId.'_'.$stevilo.'" value="-1" />';
+        }
 		return $code;
 	}
 	
-	public function text($min, $max, $fieldId, $required="0", $value=null, $dynamic=false) {
+	public function text($min, $max, $fieldId, $required="0", $value=null, $dynamic=false, $form=null) {
 		global $db;
 		if($required=="1") {
-		$this->javascript.="var element=document.getElementById('".$fieldId."');
-		if (element)  {
-		 if(element.value.length<".$min." || element.value.length>".$max.") {ok=false;element.className=element.className.replace('error','');element.className+='error';} 
-		 else
-		  {element.className=element.className.replace('error','');}}";
+            if($form!=null) {
+                $this->javascript.="var element=document.".$form.".".$fieldId.";";
+            } else {
+               $this->javascript.="var element=document.getElementById('".$fieldId."');"; 
+            }
+            $this->javascript.="if (element)  {
+             if(element.value.length<".$min." || element.value.length>".$max.") {ok=false;element.className=element.className.replace('error','');element.className+='error';} 
+             else
+              {element.className=element.className.replace('error','');}}";
 		}
 		if($dynamic && $db->is($fieldId))
 			return '<textarea maxlength="'.$max.'" name="'.$fieldId.'" id="'.$fieldId.'">'.$db->filter($fieldId).'"</textarea>';		
