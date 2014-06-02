9fe7576294cfbde7a6a17d91f8fd598beb893474
Nejc Zdovc <nejc@digitalship.eu>
2014-06-02 13:35:27 +0200

    #2093395 fixed data log and error log not showing alias domain, fixed data log view link

--- v2/pages/settings_dataLog.php
+++ v2/pages/settings_dataLog.php
@@ -2,7 +2,13 @@
 	if(!$session->isLogedIn() && !$security->checkURL()) {
 		exit;
 	}
-	$fileName=str_replace(array("www.", ":"), "", $user->domainName);
+	$domain=$user->domainName;
+	$alias=$db->get($db->query('SELECT name FROM cms_domains WHERE name="'.$_SERVER['HTTP_HOST'].'" AND alias="1" AND parentID="'.$user->domain.'"'));
+	if($alias) {
+		$domain=$alias['name'];
+	}
+	$fileName=str_replace(array("www.", ":"), "", $domain);
+	
 	$file = "../logs/data_".$fileName.".log";
 	$data="";
     if(!is_file($file)) {
@@ -21,11 +27,11 @@
 			$data=implode("", $file);
 		}
 	} else {
-		error_log("Data log doesn't existst: data_".$user->domainName.".log");
+		error_log("Data log doesn't existst: data_".$fileName.".log");
 	}
 ?>
 <div id="a_settings_error">
-<a href="/v2/logs/errorFront_<?php echo $user->domainName ?>.log" target="_blank" class="sumo2-tooltip view" style="float:right; margin:0px 10px 10px;" title="<?php echo $lang->MOD_55?>"></a>
+<a href="/v2/logs/data_<?php echo $fileName ?>.log" target="_blank" class="sumo2-tooltip view" style="float:right; margin:0px 10px 10px;" title="<?php echo $lang->MOD_55?>"></a>
 <div class="sumo2-tooltip" style="float:right; margin-bottom:10px; cursor:pointer;" onclick="sumo2.sumoSettings.CleanLog('<?php echo "data_".$fileName.".log" ?>')" title="<?php echo $lang->SETTINGS_41?>"><div style="display:block;width:16px;height:16px;background-image:url(images/css_sprite.png);background-position:-604px -1629px;"></div></div>
 <div class="sumo2-tooltip" style="float:right; margin-bottom:10px; margin-right:10px; cursor:pointer;" onclick="window.open('http://'+document.domain+'/v2/includes/download_dataLog.php?page=<?php echo $fileName?>', '_newtab');" title="<?php echo $lang->MOD_54?>"><div style="display:block;width:16px;height:16px;background-image:url(images/css_sprite.png);background-position:-540px -1629px;"></div></div>
 <textarea class="input-area" disabled="disabled" style="margin-left:2%; margin-right:2%; height:500px; width:96%; clear:both;"><?php echo $data?></textarea>
