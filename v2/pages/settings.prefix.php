efa90e624576ca6ddbfe64a9fe5b6ace36d23e21
Nejc Zdovc <nejc.zdovc@renderspace.si>
2015-05-07 07:14:40 +0200

    #3593954 Prefix fix (reverted from commit 32a13d2c263d13f60e50b10479974f26d56506dd)

--- v2/pages/settings.prefix.php
+++ v2/pages/settings.prefix.php
@@ -1,6 +1,4 @@
-<?php require_once('../initialize.php');
-    $security->checkFull();
- ?>
+<?php $security->checkFull(); ?>
 <div style="float:left; margin-top:10px;"><?php echo $lang->SETTINGS_42?>: </div>
 <form name="a_settings_add_mp" onsubmit="return false;">
 <input class="input" type="text" id="name" style="margin-left:10px; width:220px; float:left; margin-bottom:10px;" />
